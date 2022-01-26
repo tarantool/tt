1f6e8e96568346aa5fa2828cd28af560c4c3be69
Leonid Vasiliev <lvasiliev@tarantool.org>
2022-01-26 11:48:37 +0300

    instance: add close console socket after termination
    Follow-up #7

--- cli/running/lua/launcher.lua
+++ cli/running/lua/launcher.lua
@@ -35,7 +35,21 @@ local function start_instance()
     -- Preparation of the "console" socket.
     local console_sock = os.getenv('TT_CLI_CONSOLE_SOCKET')
     if console_sock ~= nil and console_sock ~= '' then
-        console.listen(console_sock)
+        local cons_listen_sock = console.listen(console_sock)
+
+        -- tarantool 1.10 does not have a trigger on terminate a process.
+        -- So the socket will be closed automatically on termination and
+        -- deleted from "running.go".
+        if box.ctl.on_shutdown ~= nil then
+            local close_sock_tr = box.ctl.on_shutdown(function()
+                box.ctl.on_shutdown(nil, close_sock_tr)
+                local res, err = pcall(cons_listen_sock.close, cons_listen_sock)
+                if not res then
+                    log.error('Failed to close console socket %s: %s', console_sock, err)
+                end
+            end)
+        end
+
     end
 
     -- Start the Instance.
