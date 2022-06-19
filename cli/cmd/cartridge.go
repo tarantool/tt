6159ee6998a4b7f0b8972532f6411056de77ecf2
Pavel Balaev <balaev@tarantool.org>
2022-06-19 18:56:42 +0300

    tt: integrate cartridge cli
    This patchset adds cartridge-cli commands to tt:
    *  admin
    *  bench
    *  build
    *  create
    *  failover
    *  pack
    *  repair
    *  replicasets
    
    Cartridge-cli must be patched in order to connect to tt cobra commands
    tree. Also before build tt we must generate all cartridge-cli codegen
    files.
    
    Closes #34

---
+++ cli/cmd/cartridge.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	cc "github.com/tarantool/cartridge-cli/cli/commands"
+)
+
+// NewCartridgeCmd chains commands from cartridge-cli to our corba tree.
+func NewCartridgeCmd() *cobra.Command {
+	var cartridgeCmd = &cobra.Command{
+		Use:   "cartridge",
+		Short: "Manage cartridge application",
+	}
+
+	cartCliCmds := []*cobra.Command{
+		cc.CartridgeCliPack,
+		cc.CartridgeCliCreate,
+		cc.CartridgeCliBuild,
+		cc.CartridgeCliAdmin,
+		cc.CartridgeCliBench,
+		cc.CartridgeCliFailover,
+		cc.CartridgeCliRepair,
+		cc.CartridgeCliReplica,
+	}
+
+	for _, cmd := range cartCliCmds {
+		cartridgeCmd.AddCommand(cmd)
+	}
+
+	return cartridgeCmd
+}
