221c4f0149d80406f00667b2e3c6e010a65c509b
better0fdead <better0fdead@tarantool.org>
2022-09-09 15:37:57 +0300

    search: add local search
    Added search of local files.
    
    Follow up #95

--- cli/cmd/search.go
+++ cli/cmd/search.go
@@ -10,6 +10,10 @@ import (
 	"github.com/tarantool/tt/cli/search"
 )
 
+var (
+	local bool
+)
+
 // NewSearchCmd creates search command.
 func NewSearchCmd() *cobra.Command {
 	var searchCmd = &cobra.Command{
@@ -22,11 +26,14 @@ func NewSearchCmd() *cobra.Command {
 			}
 		},
 	}
+	searchCmd.Flags().BoolVarP(&local, "local", "", false,
+		"search in local files")
 	return searchCmd
 }
 
 // internalSearchModule is a default search module.
 func internalSearchModule(cmdCtx *cmdcontext.CmdCtx, args []string) error {
+	var err error
 	if len(args) == 0 {
 		log.Warnf("Available programs: \n" +
 			"tarantool-ee - Enterprise tarantool\n" +
@@ -37,6 +44,11 @@ func internalSearchModule(cmdCtx *cmdcontext.CmdCtx, args []string) error {
 	if len(args) != 1 {
 		return fmt.Errorf("Incorrect arguments")
 	}
-	err := search.SearchVersions(cmdCtx, args[0])
+	if local {
+		err = search.SearchVersionsLocal(cmdCtx, args[0])
+
+	} else {
+		err = search.SearchVersions(cmdCtx, args[0])
+	}
 	return err
 }
