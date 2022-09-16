88ad15a1bc422cc2b6335dacd143d47c668277eb
better0fdead <better0fdead@tarantool.org>
2022-09-16 18:03:46 +0300

    search: add integration tests for local search
    Added integration tests for local search.
    
    Follow up #95

--- test/integration/search/test_search.py
+++ test/integration/search/test_search.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 from utils import run_command_and_get_output
@@ -20,6 +21,45 @@ def test_version_cmd(tt_cmd, tmpdir):
     assert re.search(r"Search supports only tarantool/tt", output)
 
     cmd = [tt_cmd, "search"]
+    rc, output = run_command_and_get_output(cmd, cwd=tmpdir)
+    assert rc == 0
+    assert re.search(r"Available programs: ", output)
+
+
+def test_version_cmd_local(tt_cmd, tmpdir):
+    configPath = os.path.join(tmpdir, "tarantool.yaml")
+    # Create test config
+    distfilesPath = os.path.join(tmpdir, "distfiles")
+    os.mkdir(distfilesPath)
+    with open(configPath, 'w') as f:
+        f.write('tt:\n  app:\n')
+    # Download tt and tarantool repos into distfiles directory.
+    cmd_download_tarantool = ["git", "clone",
+                              "https://github.com/tarantool/tarantool.git", "--recursive",
+                              os.path.join(distfilesPath, "tarantool")]
+    rc, _ = run_command_and_get_output(cmd_download_tarantool, cwd=tmpdir)
+    assert rc == 0
+    cmd_download_tt = ["git", "clone",
+                       "https://github.com/tarantool/tt", "--recursive",
+                       os.path.join(distfilesPath, "tt")]
+    rc, _ = run_command_and_get_output(cmd_download_tt, cwd=tmpdir)
+    assert rc == 0
+    cmd = [tt_cmd, "search", "tarantool", "--local"]
+    rc, output = run_command_and_get_output(cmd, cwd=tmpdir)
+    assert rc == 0
+    assert re.search(r"Available versions of tarantool:", output)
+
+    cmd = [tt_cmd, "search", "tt", "--local"]
+    rc, output = run_command_and_get_output(cmd, cwd=tmpdir)
+    assert rc == 0
+    assert re.search(r"Available versions of tt:", output)
+
+    cmd = [tt_cmd, "search", "git", "--local"]
+    rc, output = run_command_and_get_output(cmd, cwd=tmpdir)
+    assert rc == 1
+    assert re.search(r"Search supports only tarantool/tt", output)
+
+    cmd = [tt_cmd, "search", "--local"]
     rc, output = run_command_and_get_output(cmd, cwd=tmpdir)
     assert rc == 0
     assert re.search(r"Available programs: ", output)
