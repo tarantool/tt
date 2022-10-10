0bb6b967795c1701708b10eda95d9d41c8365c99
psergee <p_sergee@mail.ru>
2022-10-11 00:16:20 +0500

    tt: support application in current dir
    Added support of start/stop application in current directory.
    If application name passed to start/stop command is equal to
    current directory name, current directory is considered as
    application.

--- test/integration/running/test_running.py
+++ test/integration/running/test_running.py
@@ -2,6 +2,7 @@ import os
 import re
 import shutil
 import subprocess
+import tempfile
 
 from utils import run_command_and_get_output, wait_file
 
@@ -196,3 +197,134 @@ def test_clean(tt_cmd, tmpdir_with_cfg):
     assert re.search(r"• " + str(logfile), clean_out)
 
     assert os.path.exists(logfile) is False
+
+
+def test_running_base_functionality_working_dir_app(tt_cmd, tmpdir_with_cfg):
+    test_app_path_src = os.path.join(os.path.dirname(__file__), "multi_inst_app")
+
+    # Default temporary directory may have very long path. This can cause socket path buffer
+    # overflow. Create our own temporary directory.
+    with tempfile.TemporaryDirectory() as tmpdir:
+        test_app_path = os.path.join(tmpdir, "app")
+        shutil.copytree(test_app_path_src, test_app_path)
+
+
+        for subdir in ["", "app"]:
+            if subdir != "":
+                os.mkdir(os.path.join(test_app_path, "app"))
+            # Start an instance.
+            start_cmd = [tt_cmd, "start", "app"]
+            instance_process = subprocess.Popen(
+                start_cmd,
+                cwd=test_app_path,
+                stderr=subprocess.STDOUT,
+                stdout=subprocess.PIPE,
+                text=True
+            )
+            start_output = instance_process.stdout.readline()
+            assert re.search(r"Starting an instance \[(router|master|replica)\]", start_output)
+
+            # Check status.
+            for instName in ["master", "replica", "router"]:
+                print(os.path.join(test_app_path, "run", "app", instName))
+                file = wait_file(os.path.join(test_app_path, "run", "app", instName),
+                                instName + ".pid", [])
+                assert file != ""
+
+            status_cmd = [tt_cmd, "status", "app"]
+            status_rc, status_out = run_command_and_get_output(status_cmd, cwd=test_app_path)
+            assert status_rc == 0
+            assert re.search(r"RUNNING. PID: \d+.", status_out)
+
+            # Stop the application.
+            stop_cmd = [tt_cmd, "stop", "app"]
+            stop_rc, stop_out = run_command_and_get_output(stop_cmd, cwd=test_app_path)
+            assert status_rc == 0
+            assert re.search(r"The Instance \(PID = \d+\) has been terminated.", stop_out)
+
+            # Check that the process was terminated correctly.
+            instance_process_rc = instance_process.wait(1)
+            assert instance_process_rc == 0
+
+
+def test_running_instance_from_multi_inst_app(tt_cmd, tmpdir_with_cfg):
+    test_app_path_src = os.path.join(os.path.dirname(__file__), "multi_inst_app")
+
+    # Default temporary directory may have very long path. This can cause socket path buffer
+    # overflow. Create our own temporary directory.
+    with tempfile.TemporaryDirectory() as tmpdir:
+        test_app_path = os.path.join(tmpdir, "app")
+        shutil.copytree(test_app_path_src, test_app_path)
+
+        # Start an instance.
+        start_cmd = [tt_cmd, "start", "app:router"]
+        instance_process = subprocess.Popen(
+            start_cmd,
+            cwd=test_app_path,
+            stderr=subprocess.STDOUT,
+            stdout=subprocess.PIPE,
+            text=True
+        )
+        start_output = instance_process.stdout.readline()
+        assert re.search(r"Starting an instance \[router\]", start_output)
+
+        # Check status.
+        file = wait_file(os.path.join(test_app_path, "run", "app", "router"), "router.pid", [])
+        assert file != ""
+
+        status_cmd = [tt_cmd, "status", "app:router"]
+        status_rc, status_out = run_command_and_get_output(status_cmd, cwd=test_app_path)
+        assert status_rc == 0
+        assert re.search(r"router: RUNNING. PID: \d+.", status_out)
+
+        for inst in ["master", "replica"]:
+            status_cmd = [tt_cmd, "status", "app:" + inst]
+            status_rc, status_out = run_command_and_get_output(status_cmd, cwd=test_app_path)
+            assert status_rc == 0
+            assert re.search(inst + ": NOT RUNNING.", status_out)
+
+        # Stop the Instance.
+        stop_cmd = [tt_cmd, "stop", "app:router"]
+        stop_rc, stop_out = run_command_and_get_output(stop_cmd, cwd=test_app_path)
+        assert status_rc == 0
+        assert re.search(r"The Instance \(PID = \d+\) has been terminated.", stop_out)
+
+        # Check that the process was terminated correctly.
+        instance_process_rc = instance_process.wait(1)
+        assert instance_process_rc == 0
+
+
+def test_running_multi_inst_app_error_cases(tt_cmd, tmpdir_with_cfg):
+    test_app_path_src = os.path.join(os.path.dirname(__file__), "multi_inst_app")
+
+    # Default temporary directory may have very long path. This can cause socket path buffer
+    # overflow. Create our own temporary directory.
+    with tempfile.TemporaryDirectory() as tmpdir:
+        test_app_path = os.path.join(tmpdir, "app")
+        shutil.copytree(test_app_path_src, test_app_path)
+
+        # Start non-existent instance.
+        start_cmd = [tt_cmd, "start", "app:no_inst"]
+        instance_process = subprocess.Popen(
+            start_cmd,
+            cwd=test_app_path,
+            stderr=subprocess.STDOUT,
+            stdout=subprocess.PIPE,
+            text=True
+        )
+        instance_process.wait(1)
+        start_output = instance_process.stdout.readline()
+        assert re.search(r"instance\(s\) not found", start_output)
+
+        # Start app with name, which differs from base dir name.
+        start_cmd = [tt_cmd, "start", "app2"]
+        instance_process = subprocess.Popen(
+            start_cmd,
+            cwd=test_app_path,
+            stderr=subprocess.STDOUT,
+            stdout=subprocess.PIPE,
+            text=True
+        )
+        instance_process.wait(1)
+        start_output = instance_process.stdout.readline()
+        assert re.search(r"Can't find an application init file", start_output)
