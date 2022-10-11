5ad102a9714cde97f95bf9bccc9bdb0e665ea8fe
Pavel Balaev <balaev@tarantool.org>
2022-10-11 11:16:45 +0300

    tt: add patcher
    Added interface for applying patches with version check.
    
    Part of #175

---
+++ cli/install/patch.go
@@ -0,0 +1,121 @@
+package install
+
+import (
+	"os"
+
+	"github.com/tarantool/tt/cli/util"
+	"github.com/tarantool/tt/cli/version"
+)
+
+type patcher interface {
+	isApplicable(ver version.Version) bool
+	apply(srcPath string, verbose bool, logFile *os.File) error
+}
+
+type defaultPatchApplier struct {
+	patch []byte
+}
+
+func (applier defaultPatchApplier) apply(srcPath string, verbose bool, logFile *os.File) error {
+	err := util.ExecuteCommandStdin("git", verbose, logFile,
+		srcPath, applier.patch, "apply")
+
+	return err
+}
+
+type patchRange_1_to_2_6_1 struct {
+	defaultPatchApplier
+}
+
+func (patchRange_1_to_2_6_1) isApplicable(ver version.Version) bool {
+	return (ver.Major == 2 && ver.Minor == 6 && ver.Patch < 1) || ver.Major == 1
+}
+
+type patch_1_10_14 struct {
+	defaultPatchApplier
+}
+
+func (patch_1_10_14) isApplicable(ver version.Version) bool {
+	return ver.Major == 1 && ver.Minor == 10 && ver.Patch == 14
+}
+
+type patch_2_8_4 struct {
+	defaultPatchApplier
+}
+
+func (patch_2_8_4) isApplicable(ver version.Version) bool {
+	return ver.Major == 2 && ver.Minor == 8 && ver.Patch == 4
+}
+
+type patch_2_10_beta struct {
+	defaultPatchApplier
+}
+
+func (patch_2_10_beta) isApplicable(ver version.Version) bool {
+	return ver.Major == 2 && ver.Minor == 10 && ver.Release.Type == version.TypeBeta
+}
+
+type patch_2_10_0_rc1 struct {
+	defaultPatchApplier
+}
+
+func (patch_2_10_0_rc1) isApplicable(ver version.Version) bool {
+	return ver.Major == 2 && ver.Minor == 10 && ver.Patch == 0 &&
+		ver.Release.Type == version.TypeBeta && ver.Release.Num == 1
+}
+
+type patchRange_1_to_1_10_12 struct {
+	defaultPatchApplier
+}
+
+func (patchRange_1_to_1_10_12) isApplicable(ver version.Version) bool {
+	return ver.Major == 1 && ver.Minor == 10 && ver.Patch < 12
+}
+
+type patchRange_2_7_to_2_7_2 struct {
+	defaultPatchApplier
+}
+
+func (patchRange_2_7_to_2_7_2) isApplicable(ver version.Version) bool {
+	return ver.Major == 2 && ver.Minor == 7 && ver.Patch < 2
+}
+
+type patchRange_2_7_2_to_2_7_4 struct {
+	defaultPatchApplier
+}
+
+func (patchRange_2_7_2_to_2_7_4) isApplicable(ver version.Version) bool {
+	return ver.Major == 2 && ver.Minor == 7 && (ver.Patch > 1 && ver.Patch < 4)
+}
+
+type patchRange_2_8_1_to_2_8_4 struct {
+	defaultPatchApplier
+}
+
+func (patchRange_2_8_1_to_2_8_4) isApplicable(ver version.Version) bool {
+	return ver.Major == 2 && ver.Minor == 8 && (ver.Patch > 0 && ver.Patch < 4)
+}
+
+type patchRange_1_to_1_10_14 struct {
+	defaultPatchApplier
+}
+
+func (patchRange_1_to_1_10_14) isApplicable(ver version.Version) bool {
+	return ver.Major == 1 && ver.Minor == 10 && ver.Patch < 14
+}
+
+type patchRange_2_8_to_2_8_3 struct {
+	defaultPatchApplier
+}
+
+func (patchRange_2_8_to_2_8_3) isApplicable(ver version.Version) bool {
+	return ver.Major == 2 && ver.Minor == 8 && ver.Patch < 3
+}
+
+type patchRange_2_to_2_8 struct {
+	defaultPatchApplier
+}
+
+func (patchRange_2_to_2_8) isApplicable(ver version.Version) bool {
+	return ver.Major == 2 && ver.Minor < 8
+}
---
+++ cli/install/patch_test.go
@@ -0,0 +1,78 @@
+package install
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/tarantool/tt/cli/util"
+	"github.com/tarantool/tt/cli/version"
+)
+
+type patchRange_1_0_to_2_9 struct {
+	defaultPatchApplier
+}
+
+func (patchRange_1_0_to_2_9) isApplicable(ver version.Version) bool {
+	return (ver.Major == 2 && ver.Minor < 10) || ver.Major == 1
+}
+
+func (applier patchRange_1_0_to_2_9) apply(srcPath string, verbose bool, logFile *os.File) error {
+	err := util.ExecuteCommandStdin("patch", verbose, logFile,
+		srcPath, applier.patch)
+
+	return err
+}
+
+type patcherInput struct {
+	ver version.Version
+}
+
+type patcherOutput struct {
+	result *[]byte
+}
+
+func TestPatcher(t *testing.T) {
+	assert := assert.New(t)
+	testDir, err := ioutil.TempDir("/tmp", "tt-unit")
+	require.NoError(t, err)
+
+	defer os.RemoveAll(testDir)
+
+	targetData := []byte("aaa\n")
+	patchData := []byte("--- testdata.old	2022-10-11 10:32:28.011753821 +0300\n" +
+		"+++ testdata	2022-10-11 10:32:34.811754169 +0300\n" + "@@ -1 +1,2 @@\n aaa\n+bbb\n")
+	targetPath := testDir + "/testdata"
+
+	testCases := make(map[patcherInput]patcherOutput)
+
+	patchResult0 := []byte("aaa\nbbb\n")
+	testCases[patcherInput{version.Version{Major: 1, Minor: 2}}] = patcherOutput{&patchResult0}
+
+	patchResult1 := []byte("aaa\n")
+	testCases[patcherInput{version.Version{Major: 3, Minor: 0}}] = patcherOutput{&patchResult1}
+
+	for input, output := range testCases {
+		targetFile, err := os.Create(targetPath)
+		require.NoError(t, err)
+
+		_, err = targetFile.Write(targetData)
+		require.NoError(t, err)
+		targetFile.Close()
+
+		patch := patchRange_1_0_to_2_9{defaultPatchApplier{patchData}}
+
+		if patch.isApplicable(input.ver) {
+			patch.apply(testDir, false, nil)
+		}
+
+		patchedData, err := os.ReadFile(targetPath)
+		require.NoError(t, err)
+
+		assert.Equal(*output.result, patchedData)
+
+		os.Remove(targetPath)
+	}
+}
