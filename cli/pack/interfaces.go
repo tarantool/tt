b30a0b3d7907884a5cb343a62fb37a91b47b5438
psergee <p_sergee@mail.ru>
2022-10-17 12:17:45 +0500

    tt: separate pack context
    Part of #137

--- cli/pack/interfaces.go
+++ cli/pack/interfaces.go
@@ -4,5 +4,5 @@ import "github.com/tarantool/tt/cli/cmdcontext"
 
 // Packer is an interface that packs an app.
 type Packer interface {
-	Run(cmdCtx *cmdcontext.CmdCtx) error
+	Run(cmdCtx *cmdcontext.CmdCtx, packCtx *cmdcontext.PackCtx) error
 }
