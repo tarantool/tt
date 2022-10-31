50ce8f3d5e5518166753f8bb71ef0dc8e834fc1b
psergee <p_sergee@mail.ru>
2022-10-31 19:33:53 +0500

    tt: support yaml/yml config file extensions

--- cli/create/internal/steps/load_manifest.go
+++ cli/create/internal/steps/load_manifest.go
@@ -8,6 +8,7 @@ import (
 	"github.com/apex/log"
 	create_ctx "github.com/tarantool/tt/cli/create/context"
 	"github.com/tarantool/tt/cli/create/internal/app_template"
+	"github.com/tarantool/tt/cli/util"
 )
 
 // LoadManifest represents manifest load step.
@@ -16,14 +17,14 @@ type LoadManifest struct {
 
 // Run loads template manifest. Missing manifest is not an error.
 func (LoadManifest) Run(ctx *create_ctx.CreateCtx, templateCtx *app_template.TemplateCtx) error {
-	manifestPath := path.Join(templateCtx.AppPath, app_template.DefaultManifestName)
-
-	if _, err := os.Stat(manifestPath); err != nil {
-		if os.IsNotExist(err) {
-			log.Info("There is no manifest in template.")
-			templateCtx.IsManifestPresent = false
-			return nil
-		}
+	manifestPath, err := util.FindYamlFile(path.Join(templateCtx.AppPath,
+		app_template.DefaultManifestName))
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	} else if os.IsNotExist(err) {
+		log.Info("There is no manifest in template.")
+		templateCtx.IsManifestPresent = false
+		return nil
 	}
 
 	manifest, err := app_template.LoadManifest(manifestPath)
