20c4e14543168e7976593b5948be9b2762a5455c
psergee <p_sergee@mail.ru>
2022-12-01 12:47:23 +0300

    run: update help, add examples
    Part of #246

--- cli/cmd/run.go
+++ cli/cmd/run.go
@@ -44,11 +44,12 @@ func newRunOpts(cmdCtx cmdcontext.CmdCtx) *running.RunOpts {
 // NewRunCmd creates run command.
 func NewRunCmd() *cobra.Command {
 	var runCmd = &cobra.Command{
-		Use:   "run [APPLICATION_NAME]",
-		Short: "Run tarantool instance",
-		Long: "Run tarantool instance\n" +
-			"Flags processed within the application\n" +
-			"are passed after: '--'",
+		Use:   "run [SCRIPT.lua [flags] [-- ARGS]]",
+		Short: "Run Tarantool instance",
+		Long: `Run Tarantool instance.
+All command line arguments are passed to the interpreted SCRIPT. Options to process in the SCRIPT
+are passed after '--'.
+`,
 		Run: func(cmd *cobra.Command, args []string) {
 			cmdCtx.CommandName = cmd.Name()
 			err := modules.RunCmd(&cmdCtx, cmd.Name(), &modulesInfo, internalRunModule, args)
@@ -56,7 +57,38 @@ func NewRunCmd() *cobra.Command {
 				log.Fatalf(err.Error())
 			}
 		},
+		Example: `
+# Print current environment Tarantool version:
+
+    $ tt run --version
+    Tarantool 2.11.0-entrypoint-724-gd2d7f4de3
+    . . .
+
+# Run a script (which print passed arguments) with 3 arguments and 2 options:
+
+    $ run script.lua a b c -- -a -b
+    a	b	c	-a	-b
+
+# Run a script, pass '-i' argument to it, and enter interactive mode after script execution:
+
+    $ tt run -i script.lua -- -i
+    -i
+    Tarantool 2.11.0-entrypoint-724-gd2d7f4de3
+    type 'help' for interactive help
+    tarantool>
+
+First '-i' option is parsed by 'tt run' and means 'enter interactive mode'. The second '-i'
+is after '--', so passed to script.lua as is.
+
+# Execute stdin:
+
+    $ echo 'print(42)' | tt run -
+    42
+
+`,
+		DisableFlagsInUseLine: true,
 	}
+
 	runCmd.Flags().StringVarP(&runEval, "evaluate", "e", "", "execute string 'EXPR'")
 	runCmd.Flags().StringVarP(&runLib, "library", "l", "", "require library 'NAME'")
 	runCmd.Flags().BoolVarP(&runInteractive, "interactive", "i", false,
