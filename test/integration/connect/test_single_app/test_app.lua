d96301f3acbd3c38ecf49ee1477cdbe6ddf91928
Oleg Jukovec <oleg.jukovec@tarantool.org>
2023-03-20 14:58:56 +0300

    connect: fix change language race in tests
    Connect integration tests with change languages workflow:
    
    1. Start tarantool with app script.
    2. Connect to started instance with -l option.
    
    There is a race between console socket creation and box.cfg call. The
    second step may fail with the following error:
    
        error: Unable to set language to 'sql' with unconfigured box
    
    because the socket is already created, but box.cfg from application
    script is not called yet.
    
    The patch creates an additional file `configured` after `box.cfg({})`
    to ensure that is is already complited.
    
    Closes #313

--- test/integration/connect/test_single_app/test_app.lua
+++ test/integration/connect/test_single_app/test_app.lua
@@ -1,9 +1,14 @@
 local fiber = require('fiber')
+local fio = require('fio')
 
 box.cfg({})
 
 box.schema.user.create('test', { password = 'password' , if_not_exists = true })
 box.schema.user.grant('test','read,write,execute,create,drop','universe')
+
+-- \set language works only after box.cfg({}). The file helps to ensure that
+-- box.cfg({}) is already done.
+fio.open('configured', 'O_CREAT'):close()
 
 while true do
     fiber.sleep(5)
