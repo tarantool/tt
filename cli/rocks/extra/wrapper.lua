5db039e815af2d836880989d3ba3b81acf9a4a2d
psergee <p_sergee@mail.ru>
2023-03-30 13:20:02 +0300

    rocks: disable init command
    Closes #358

--- cli/rocks/extra/wrapper.lua
+++ cli/rocks/extra/wrapper.lua
@@ -12,14 +12,19 @@ local function exec(bin, ...)
         return bin .. " rocks"
     end
 
-    -- Disabled: path, upload.
+    --[[ Disabled: path, upload,
+    -- init: luarocks init command generates a project, including local
+    dependency management. It also creates two wrapper scripts that can be used to run
+    lua & luarocks from inside the project. tt rocks init is unable to create correct luarocks
+    wrapper because luarocks script, that must be wrapped, is missing.
+    So, rocks init is disabled for now.
+    --]]
     local rocks_commands = {
         build = "luarocks.cmd.build",
         config = "luarocks.cmd.config",
         doc = "luarocks.cmd.doc",
         download = "luarocks.cmd.download",
         help = "luarocks.cmd.help",
-        init = "luarocks.cmd.init",
         install = "luarocks.cmd.install",
         lint = "luarocks.cmd.lint",
         list = "luarocks.cmd.list",
