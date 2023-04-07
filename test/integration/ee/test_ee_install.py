0cb7d9f703cc1b22a4fc57449add8f6b601e6d9b
Rianov Viacheslav <slavarianov@yandex.ru>
2023-04-07 05:37:52 +0300

    ee: add ee integration tests
    With this patch:
    - added integration tests for search/install tarantool-ee
    - added a new tag for ee tests
    - included ee tests in full-ci target
    - moved ee integration tests into a separate job in CI
    
    Closes #357

---
+++ test/integration/ee/test_ee_install.py
@@ -0,0 +1,37 @@
+import os
+import re
+
+import pytest
+
+from utils import run_command_and_get_output
+
+# ##### #
+# Tests #
+# ##### #
+
+
+@pytest.mark.slow_ee
+def test_install_ee(tt_cmd, tmpdir):
+    rc, output = run_command_and_get_output(
+        [tt_cmd, "init"],
+        cwd=tmpdir, env=dict(os.environ, PWD=tmpdir))
+    assert rc == 0
+
+    rc, output = run_command_and_get_output(
+        [tt_cmd, "search", "tarantool-ee"],
+        cwd=tmpdir, env=dict(os.environ, PWD=tmpdir))
+
+    version = output.split('\n')[1]
+    assert re.search(r"(\d+.\d+.\d+|<unknown>)",
+                     version)
+
+    rc, output = run_command_and_get_output(
+        [tt_cmd, "install", "tarantool-ee="+version],
+        cwd=tmpdir, env=dict(os.environ, PWD=tmpdir))
+
+    assert rc == 0
+    assert re.search("Installing tarantool-ee="+version, output)
+    assert re.search("Downloading tarantool-ee...", output)
+    assert re.search("Done.", output)
+    assert os.path.exists(os.path.join(tmpdir, 'bin', 'tarantool'))
+    assert os.path.exists(os.path.join(tmpdir, 'include', 'include', 'tarantool'))
---
+++ test/integration/ee/test_ee_search.py
@@ -0,0 +1,24 @@
+import os
+import re
+
+import pytest
+
+from utils import run_command_and_get_output
+
+# ##### #
+# Tests #
+# ##### #
+
+
+@pytest.mark.slow_ee
+def test_search_ee(tt_cmd, tmpdir):
+    cmds = [
+        [tt_cmd, "search", "tarantool-ee"],
+        [tt_cmd, "search", "tarantool-ee", "--dev"],
+    ]
+    for cmd in cmds:
+        rc, output = run_command_and_get_output(
+            cmd, cwd=tmpdir, env=dict(os.environ, PWD=tmpdir))
+        assert re.search(r"(\d+.\d+.\d+)",
+                         output)
+        assert rc == 0
