62cae7140547f061912cb92579ec0644754abb5e
psergee <p_sergee@mail.ru>
2023-08-28 16:11:57 +0300

    util: simple var applying functionality

---
+++ cli/util/regexputil/regexp.go
@@ -0,0 +1,33 @@
+package regexputil
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"golang.org/x/exp/maps"
+)
+
+var varPattern = regexp.MustCompile(`{{\s*([^ ]+)\s*}}`)
+
+// ApplyVars replaces '{{ key }}' in str string by a value from the data map.
+func ApplyVars(templateStr string, data map[string]string) (string, error) {
+	var missingVars = make(map[string]bool, 0)
+	renderedStr := varPattern.ReplaceAllStringFunc(templateStr, func(varNameStr string) string {
+		if subMatches := varPattern.FindStringSubmatch(varNameStr); subMatches != nil {
+			if val, found := data[subMatches[1]]; !found {
+				missingVars[subMatches[1]] = true
+			} else {
+				return val
+			}
+		}
+		return varNameStr
+	})
+
+	if len(missingVars) > 0 {
+		return renderedStr, fmt.Errorf("missing vars: %s\nin template string: %q",
+			strings.Join(maps.Keys(missingVars), ","), templateStr)
+	}
+
+	return renderedStr, nil
+}
