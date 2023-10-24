202ce2909ea0253b97e9f8957e1a4335df8316d8
Oleg Jukovec <oleg.jukovec@tarantool.org>
2023-10-24 16:43:33 +0300

    cluster: add missed credentials pass methods
    The patch adds etcd credentials pass via environment variables
    and command flags.
    
    Part of https://github.com/tarantool/roadmap-internal/issues/281

--- cli/connect/const.go
+++ cli/connect/const.go
@@ -8,6 +8,12 @@ const TarantoolUsernameEnv = "TT_CLI_USERNAME"
 // Tarantool.
 const TarantoolPasswordEnv = "TT_CLI_PASSWORD"
 
+// EtcdUsernameEnv is an environment variable with a username for etcd.
+const EtcdUsernameEnv = "TT_CLI_ETCD_USERNAME"
+
+// EtcdPasswordEnv is an environment variable with a password for etcd.
+const EtcdPasswordEnv = "TT_CLI_ETCD_PASSWORD"
+
 // setLanguagePrefix is used to set a language in the Tarantool console.
 const setLanguagePrefix = "\\set language"
 
