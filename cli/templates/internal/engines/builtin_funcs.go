0442a40ff7db491c80c3ece12aacb7c5cc8d3b7f
Albert Skalt <askalt@tarantool.org>
2023-12-27 18:24:57 +0300

    templates: add built-in functions to the engine
    This patch adds several useful built-in functions
    to the template engine, which can be used in templates.
    
    * `port` generates a port
    * `atoi` converts string to int
    * `replicasets` generates names for replicasets and instances
    
    Part of #489

---
+++ cli/templates/internal/engines/builtin_funcs.go
@@ -0,0 +1,62 @@
+package engines
+
+import (
+	"fmt"
+)
+
+// genState describes template generation state.
+type genState struct {
+	port int
+}
+
+// newGenState creates genState.
+func newGenState() *genState {
+	return &genState{port: 3301}
+}
+
+// genPort generates port.
+func (state *genState) genPort() int {
+	ret := state.port
+	state.port++
+	return ret
+}
+
+// replicaset describes generated replicaset information.
+type replicaset struct {
+	Name      string
+	InstNames []string
+}
+
+const (
+	maxReplicasetsNumber = 1000
+	maxReplicasetSize    = 32
+)
+
+// genReplicasets generates replicasets bunch.
+func genReplicasets(
+	baseName string,
+	replicasetsNumber int,
+	replicasetSize int) ([]replicaset, error) {
+	if replicasetsNumber <= 0 || replicasetsNumber > maxReplicasetsNumber {
+		return nil, fmt.Errorf("replicasetsNumber must be in [%d, %d]", 0, maxReplicasetsNumber)
+	}
+	if replicasetSize <= 0 || replicasetSize > maxReplicasetSize {
+		return nil, fmt.Errorf("replicasetSize must be in [%d, %d]", 0, maxReplicasetSize)
+	}
+
+	replicasets := make([]replicaset, replicasetsNumber)
+	for i := range replicasets {
+		replicasetName := fmt.Sprintf("%s-%03d", baseName, i+1)
+		instNames := make([]string, replicasetSize)
+		for j := range instNames {
+			if replicasetSize <= 26 {
+				instNames[j] = fmt.Sprintf("%s-%c", replicasetName, 'a'+byte(j))
+			} else {
+				instNames[j] = fmt.Sprintf("%s-%03d", replicasetName, j+1)
+			}
+		}
+		replicasets[i].Name = replicasetName
+		replicasets[i].InstNames = instNames
+	}
+	return replicasets, nil
+}
