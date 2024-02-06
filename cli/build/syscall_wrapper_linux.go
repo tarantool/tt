fa12f80805bf5f5a78e4f17039f02c6b6805701c
psergee <p_sergee@mail.ru>
2024-02-06 17:20:56 +0300

    build: fix invalid argument error in verbose mode
    If verbose mode is enabled, dup3 is called with the same file
    descriptors (stdout for old and new), which causes "invalid argument"
    error. Skip calling `dup3` if descriptors are equal.

--- cli/build/syscall_wrapper_linux.go
+++ cli/build/syscall_wrapper_linux.go
@@ -6,6 +6,10 @@ import (
 	"syscall"
 )
 
+// Dup2 is a dup3 syscall wrapper to use on Linux instead of dup2 syscall.
 func Dup2(oldfd int, newfd int) (err error) {
-	return syscall.Dup3(oldfd, newfd, 0)
+	if oldfd != newfd { // dup2 should not fail if FDs are equal.
+		return syscall.Dup3(oldfd, newfd, 0)
+	}
+	return nil
 }
