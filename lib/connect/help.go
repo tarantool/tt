2b3be97f73cdd83c6ddb9f1fa57bdb7fb97364b2
Georgy Moiseev <moiseev.georgii@gmail.com>
2024-05-06 17:59:28 +0300

    libconnect: extract common help info
    Environment help is already reused in several places, centralized
    config info is planned to be reused in [1].
    
    1. https://github.com/tarantool/tt-ee/issues/171

---
+++ lib/connect/help.go
@@ -0,0 +1,5 @@
+package connect
+
+const EnvCredentialsHelp = "The command supports the following environment variables:\n\n" +
+	"* " + TarantoolUsernameEnv + " - specifies a username\n" +
+	"* " + TarantoolPasswordEnv + " - specifies a password"
