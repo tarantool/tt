1275a469b7716801041e99fe908140692ef6547d
Mikhail Elhimov <m.elhimov@vk.team>
2025-01-21 01:00:53 +0300

    uninstall: reuse `GetAvailableVersions`

--- cli/uninstall/uninstall_test.go
+++ cli/uninstall/uninstall_test.go
@@ -1,71 +1,108 @@
 package uninstall
 
 import (
-	"io"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/tarantool/tt/cli/configure"
 	"github.com/tarantool/tt/cli/search"
 	"github.com/tarantool/tt/cli/version"
 )
-
-const testDirName = "uninstall-test-dir"
-
-type mockRepository struct{}
-
-func (mock *mockRepository) Read(path string) (io.ReadCloser, error) {
-	return os.Open(path)
-}
-
-func (mock *mockRepository) ValidateAll() error {
-	return nil
-}
 
 func TestGetAvailableVersions(t *testing.T) {
 	assert := assert.New(t)
 	workDir := t.TempDir()
 
-	binDir := filepath.Join(workDir, "bin")
-	err := os.Mkdir(binDir, os.ModePerm)
-	require.NoError(t, err)
-
-	cfgData := []byte("tt:\n  app:\n    bin_dir: " + binDir)
-	cfgPath := filepath.Join(workDir, "tt.yaml")
-
-	err = os.WriteFile(cfgPath, cfgData, 0400)
-	require.NoError(t, err)
-
-	files := []string{
-		"tt" + version.FsSeparator + "1.2.3",
-		"tarantool" + version.FsSeparator + "1.2.10",
-		"tarantool-ee" + version.FsSeparator + "master",
+	type testCase struct {
+		name     string
+		program  string
+		dir      string
+		expected []string
+		isErr    bool
 	}
-	for _, file := range files {
-		f, err := os.Create(filepath.Join(binDir, file))
+	base_cases := []testCase{
+		{
+			program: search.ProgramTt,
+			expected: []string{
+				"1.1.0",
+				"1.2.3",
+				"1.3.4",
+				"2.1.0",
+			},
+		},
+		{
+			program: search.ProgramCe,
+			expected: []string{
+				"1.10.0",
+				"2.10.8",
+				"2.11.8",
+				"3.1.2",
+				"master",
+				"aaaaaaa",
+				"cececece",
+			},
+		},
+		{
+			program: search.ProgramEe,
+			expected: []string{
+				"1.10.15-0-r543",
+				"2.8.4-0-r510",
+				"gc64-2.11.5-0-r662",
+				"gc64-3.3.0-0-r43",
+				"master",
+				"bbbbbbb",
+				"eeeeeee",
+			},
+		},
+	}
+
+	// Setup test cases
+	cases := []testCase{}
+	for _, set_name := range []string{"empty", "single", "multiple"} {
+		dir := filepath.Join(workDir, "dir_"+set_name)
+		err := os.Mkdir(dir, os.ModePerm)
 		require.NoError(t, err)
-		f.Close()
+		for _, base_case := range base_cases {
+			tc := base_case
+			tc.name = set_name + "_" + base_case.program
+			tc.dir = dir
+			switch set_name {
+			case "empty":
+				tc.expected = []string{}
+			case "single":
+				tc.expected = base_case.expected[0:1]
+			case "multiple":
+				tc.expected = base_case.expected
+			}
+			for _, ver := range tc.expected {
+				fileName := tc.program + version.FsSeparator + ver
+				f, err := os.Create(filepath.Join(dir, fileName))
+				require.NoError(t, err)
+				f.Close()
+			}
+			cases = append(cases, tc)
+		}
 	}
 
-	cliOpts, _, err := configure.GetCliOpts(cfgPath, &mockRepository{})
-	require.NoError(t, err)
-	result := GetAvailableVersions("tt", cliOpts.Env.BinDir)
-	assert.Equal(result, []string{"1.2.3"})
-
-	result = GetAvailableVersions("tarantool", cliOpts.Env.BinDir)
-	assert.Equal(result, []string{"1.2.10"})
-
-	result = GetAvailableVersions("tarantool-ee", cliOpts.Env.BinDir)
-	assert.Equal(result, []string{"master"})
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			versions, err := GetAvailableVersions(tc.program, tc.dir)
+			if !tc.isErr {
+				assert.NoError(err)
+				assert.Equal(tc.expected, versions)
+			} else {
+				assert.Error(err)
+			}
+		})
+	}
 }
 
 func TestSearchLatestVersion(t *testing.T) {
 	type testCase struct {
 		name        string
-		linkName    string
+		program     string
 		binDst      string
 		headerDst   string
 		expectedVer string
@@ -75,42 +112,42 @@ func TestSearchLatestVersion(t *testing.T) {
 	cases := []testCase{
 		{
 			name:        "basic",
-			linkName:    "tarantool",
+			program:     "tarantool",
 			binDst:      "./testdata/bin_basic",
 			headerDst:   "./testdata/inc_basic",
 			expectedVer: "tarantool_3.0.0-entrypoint",
 		},
 		{
 			name:        "no includes",
-			linkName:    "tarantool",
+			program:     "tarantool-ee",
 			binDst:      "./testdata/bin_basic",
 			headerDst:   "./testdata/inc_invalid",
 			expectedVer: "tarantool-ee_2.8.4-0-r510",
 		},
 		{
 			name:        "tarantool-dev",
-			linkName:    "tarantool",
+			program:     "tarantool-dev",
 			binDst:      "./testdata/bin_dev",
 			headerDst:   "./testdata/inc_basic",
 			expectedVer: "",
 		},
 		{
 			name:        "hash version",
-			linkName:    "tarantool",
+			program:     "tarantool",
 			binDst:      "./testdata/bin_hash",
 			headerDst:   "./testdata/inc_hash",
 			expectedVer: "tarantool_aaaaaaa",
 		},
 		{
 			name:        "hash invalid headers",
-			linkName:    "tarantool",
+			program:     "tarantool",
 			binDst:      "./testdata/bin_hash",
 			headerDst:   "./testdata/inc_invalid_hash",
 			expectedVer: "tarantool_bbbbbbb",
 		},
 		{
 			name:        "tt, include-dir basic",
-			linkName:    "tt",
+			program:     "tt",
 			binDst:      "./testdata/bin_basic",
 			headerDst:   "./testdata/inc_basic",
 			expectedVer: "tt_2.0.0",
@@ -118,14 +155,14 @@ func TestSearchLatestVersion(t *testing.T) {
 		// Test that include dir doesn't affect the search for `tt`.
 		{
 			name:        "tt, include-dir invalid",
-			linkName:    "tt",
+			program:     "tt",
 			binDst:      "./testdata/bin_basic",
 			headerDst:   "./testdata/inc_invalid",
 			expectedVer: "tt_2.0.0",
 		},
 		{
 			name:      "filename as a bin dir",
-			linkName:  "tt",
+			program:   "tt",
 			binDst:    "./testdata/bin_basic/tarantool",
 			headerDst: "./testdata/inc_basic",
 			isErr:     true,
@@ -134,7 +171,7 @@ func TestSearchLatestVersion(t *testing.T) {
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			ver, err := searchLatestVersion(tc.linkName, tc.binDst, tc.headerDst)
+			ver, err := searchLatestVersion(tc.program, tc.binDst, tc.headerDst)
 			if !tc.isErr {
 				assert.NoError(t, err)
 				assert.Equal(t, tc.expectedVer, ver)
