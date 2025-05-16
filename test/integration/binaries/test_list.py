922d3ee5ff99e64835202bdb6c91179aefdfe7ea
Dmitriy Gertsog <dmyger@gmail.com>
2025-05-16 20:03:01 +0300

    binaries: update integration tests
    Part of #TNTP-2764

--- test/integration/binaries/test_list.py
+++ test/integration/binaries/test_list.py
@@ -1,150 +1,139 @@
-import os
-import shutil
-import subprocess
+import re
+from pathlib import Path
+from shutil import copyfile, copytree
 
 from utils import config_name, run_command_and_get_output
 
+DATA_DIR = Path(__file__).parent / "testdata"
+ANSI_ESCAPE = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
+
+
+def cleanup_ansi(text):
+    return ANSI_ESCAPE.sub("", text)
+
+
+def compare_output(actual: str, expected: str) -> bool:
+    """
+    Compare the output of the command with the expected output.
+    """
+    # Cleaning lines. Remove leading, trailing whitespace and ANSI escape codes.
+    got_lines = [cleanup_ansi(line.strip()) for line in actual.splitlines()]
+    expected_lines = [line.strip() for line in expected.splitlines()]
+
+    return got_lines == expected_lines
+
 
 def test_list(tt_cmd, tmp_path):
-    # Copy the test bin_dir to the "run" directory.
-    test_app_path = os.path.join(os.path.dirname(__file__), "bin")
-    shutil.copytree(test_app_path, tmp_path / "bin", True)
+    copytree(DATA_DIR / "list", tmp_path, symlinks=True, dirs_exist_ok=True)
 
-    configPath = tmp_path / config_name
-    # Create test config
-    with open(configPath, 'w') as f:
-        f.write('tt:\n  env:\n    bin_dir: "./bin"\n    inc_dir:\n')
+    rc, output = run_command_and_get_output([tt_cmd, "binaries", "list"], cwd=tmp_path)
 
-    # Print binaries
-    binaries_cmd = [tt_cmd, "--cfg", configPath, "binaries", "list"]
-    binaries_process = subprocess.Popen(
-        binaries_cmd,
-        cwd=tmp_path,
-        stderr=subprocess.STDOUT,
-        stdout=subprocess.PIPE,
-        text=True
-    )
-
-    binaries_process.wait()
-    output = binaries_process.stdout.read()
-
-    assert "tt" in output
-    assert "0.1.0" in output
-    assert "tarantool" in output
-    assert "2.10.3" in output
-    assert "2.8.1 [active]" in output
+    expected_output = """List of installed binaries:
+   • tt:
+    0.1.0
+   • tarantool:
+    2.10.3
+    2.8.1 [active]
+   • tcm:
+    1.3.1-0-g074b5ffa [active]
+    1.2.0-11-g2d0a0f495
+"""
+    assert rc == 0
+    assert compare_output(output, expected_output)
 
 
 def test_list_no_directory(tt_cmd, tmp_path):
-    configPath = tmp_path / config_name
     # Create test config
-    with open(configPath, 'w') as f:
-        f.write('tt:\n  env:\n    bin_dir: "./bin"\n    inc_dir:\n')
+    copyfile(DATA_DIR / "list" / config_name, tmp_path / config_name)
 
     # Print binaries
-    binaries_cmd = [tt_cmd, "--cfg", configPath, "binaries", "list"]
-    binaries_process = subprocess.Popen(
-        binaries_cmd,
-        cwd=tmp_path,
-        stderr=subprocess.STDOUT,
-        stdout=subprocess.PIPE,
-        text=True
-    )
+    rc, output = run_command_and_get_output([tt_cmd, "binaries", "list"], cwd=tmp_path)
 
-    binaries_process.wait()
-    output = binaries_process.stdout.read()
-
+    assert rc == 1
     assert "there are no binaries installed in this environment of 'tt'" in output
 
 
 def test_list_empty_directory(tt_cmd, tmp_path):
-    configPath = tmp_path / config_name
-    os.mkdir(tmp_path / "bin")
-    # Create test config
-    with open(configPath, 'w') as f:
-        f.write('tt:\n  env:\n    bin_dir: "./bin"\n    inc_dir:\n')
+    # Create test config and empty bin directory.
+    copyfile(DATA_DIR / "list" / config_name, tmp_path / config_name)
+    (tmp_path / "bin").mkdir()
 
     # Print binaries
-    binaries_cmd = [tt_cmd, "--cfg", configPath, "binaries", "list"]
-    binaries_process = subprocess.Popen(
-        binaries_cmd,
-        cwd=tmp_path,
-        stderr=subprocess.STDOUT,
-        stdout=subprocess.PIPE,
-        text=True
-    )
+    rc, output = run_command_and_get_output([tt_cmd, "binaries", "list"], cwd=tmp_path)
 
-    binaries_process.wait()
-    output = binaries_process.stdout.read()
-
+    assert rc == 1
     assert "there are no binaries installed in this environment of 'tt'" in output
 
 
 def test_list_tarantool_dev(tt_cmd, tmp_path):
     # Copy the test dir to the "run" directory.
-    test_app_path = os.path.join(os.path.dirname(__file__), "tarantool_dev")
-    shutil.copytree(test_app_path, tmp_path / "tarantool_dev", True)
+    copytree(DATA_DIR / "tarantool_dev", tmp_path, symlinks=True, dirs_exist_ok=True)
 
-    config_path = tmp_path / config_name
-    # Create test config.
-    with open(config_path, "w") as f:
-        f.write(
-            'env:\n  bin_dir: "./tarantool_dev/bin"\n  inc_dir:\n')
+    rc, output = run_command_and_get_output([tt_cmd, "binaries", "list"], cwd=tmp_path)
 
-    binaries_cmd = [tt_cmd, "--cfg", config_path, "binaries", "list"]
-    binaries_process = subprocess.Popen(
-        binaries_cmd,
-        cwd=tmp_path,
-        stderr=subprocess.STDOUT,
-        stdout=subprocess.PIPE,
-        text=True
-    )
+    expected_output = f"""List of installed binaries:
+   • tarantool:
+    2.10.7
+    1.10.0
+   • tarantool-dev:
+    tarantool-dev -> {tmp_path}/tarantool [active]
+"""
+    assert rc == 0
+    assert compare_output(output, expected_output)
 
-    binaries_process.wait()
-    output = binaries_process.stdout.read()
 
-    assert "2.10.7" in output
-    assert "1.10.0" in output
-    assert "tarantool-dev" in output
+def replace_prog(prog: Path, version: str) -> None:
+    prog.unlink(missing_ok=True)
+    with open(prog, "w") as p:
+        p.write(
+            f"""#!/bin/sh
+echo '{version}'
+"""
+        )
+    prog.chmod(0o750)
 
 
 def test_list_tarantool_no_symlink(tt_cmd, tmp_path):
     # Copy the test bin_dir to the "run" directory.
-    test_app_path = os.path.join(os.path.dirname(__file__), "bin")
-    shutil.copytree(test_app_path, tmp_path / "bin", True)
+    copytree(DATA_DIR / "list", tmp_path, symlinks=True, dirs_exist_ok=True)
 
-    configPath = tmp_path / config_name
-    # Create test config
-    with open(configPath, 'w') as f:
-        f.write('tt:\n  env:\n    bin_dir: "./bin"\n    inc_dir:\n')
-
-    os.remove(tmp_path / "bin" / "tarantool")
-    with open(tmp_path / "bin" / "tarantool", "w") as tnt_file:
-        tnt_file.write("""#!/bin/sh
-echo 'Tarantool 3.1.0-entrypoint-83-gcb0264c3c'""")
-    os.chmod(tmp_path / "bin" / "tarantool", 0o750)
+    replace_prog(
+        tmp_path / "bin" / "tarantool", "Tarantool 3.1.0-entrypoint-83-gcb0264c3c"
+    )
 
     # Print binaries
-    binaries_cmd = [tt_cmd, "--cfg", configPath, "binaries", "list"]
-    rc, output = run_command_and_get_output(binaries_cmd, cwd=tmp_path)
+    rc, output = run_command_and_get_output([tt_cmd, "binaries", "list"], cwd=tmp_path)
+
+    expected_output = """List of installed binaries:
+   • tt:
+    0.1.0
+   • tarantool:
+    3.1.0-entrypoint-83-gcb0264c3c [active]
+    2.10.3
+    2.8.1
+   • tcm:
+    1.3.1-0-g074b5ffa [active]
+    1.2.0-11-g2d0a0f495
+"""
 
     assert rc == 0
-    assert "tt" in output
-    assert "0.1.0" in output
-    assert "tarantool:" in output
-    assert "2.10.3" in output
-    assert "2.8.1" in output
-    assert "3.1.0-entrypoint-83-gcb0264c3c [active]" in output
+    assert compare_output(output, expected_output)
 
-    # Remove non-versioned tarantool binary.
-    os.remove(tmp_path / "bin" / "tarantool")
-    binaries_cmd = [tt_cmd, "--cfg", configPath, "binaries", "list"]
-    rc, output = run_command_and_get_output(binaries_cmd, cwd=tmp_path)
+    # Remove non-versioned tarantool binary and TCM symlink.
+    (tmp_path / "bin" / "tarantool").unlink(missing_ok=True)
+    (tmp_path / "bin" / "tcm").unlink(missing_ok=True)
+    rc, output = run_command_and_get_output([tt_cmd, "binaries", "list"], cwd=tmp_path)
+
+    expected_output = """List of installed binaries:
+   • tt:
+    0.1.0
+   • tarantool:
+    2.10.3
+    2.8.1
+   • tcm:
+    1.3.1-0-g074b5ffa
+    1.2.0-11-g2d0a0f495
+"""
 
     assert rc == 0
-    assert "tt" in output
-    assert "0.1.0" in output
-    assert "tarantool:" in output
-    assert "2.10.3" in output
-    assert "2.8.1" in output
-    assert "[active]" not in output  # No active tarantool.
+    assert compare_output(output, expected_output)
