813a9b579f4ef3e63b996cf8db37d4cd9ae329b4
Eugene Blikh <bigbes@gmail.com>
2026-05-15 19:44:10 +0300

    lib/cluster: drop in-tree config, collector, publisher, yaml modules
    These files implemented the in-tree ClusterConfig hierarchy, the
    Collector/CollectorFactory layer, and the YAML decorator that consumers
    have all moved off of.
    
    Closes TNTP-7390

--- lib/cluster/publisher.go
+++
@@ -1,7 +0,0 @@
-package cluster
-
-// ConfigPublisher interface must be implemented by a config publisher.
-type ConfigPublisher interface {
-	// Publish publisher the configuration or returns an error.
-	Publish(config *Config) error
-}
