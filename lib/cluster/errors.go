96c6768dd3f8acbfcfba2934c7841dda4988889f
Eugene Blikh <bigbes@gmail.com>
2026-05-15 19:43:49 +0300

    lib/cluster: trim cluster.go and integration tests to goconfig API
    CreateDataCollector is rewritten on top of NewStorageConnection +
    NewRemoteStorage so that callers no longer need to know about etcd/TCS
    client primitives. Integration tests are updated to drop the removed
    YamlCollectorDecorator wrapper and to take a pkgstorage.Storage instead
    of a raw etcd client.
    
    Part of TNTP-7390

--- lib/cluster/errors.go
+++ lib/cluster/errors.go
@@ -5,16 +5,6 @@ import (
 	"fmt"
 )
 
-// NotExistError error type for non-existing path.
-type NotExistError struct {
-	path []string
-}
-
-// Error - error interface implementation for NotExistError.
-func (e NotExistError) Error() string {
-	return fmt.Sprintf("path %q does not exist", e.path)
-}
-
 // CollectEmptyError responses on DataCollector.Collect() if
 // config was not found with specified prefix.
 type CollectEmptyError struct {
